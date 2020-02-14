b181fc93f90ced86da42cb28661416021fab000c
Ramses Cruz <rcru@devolabs.io>
2020-02-13 21:22:45 -0500

    Agregado Connection PGSQL

--- config/database.php
+++ config/database.php
@@ -34,7 +34,18 @@ return [
     */
 
     'connections' => [
-
+      'heroku_pgsql' => [
+       'driver' => 'pgsql',
+       'host' => @parse_url(getenv("DATABASE_URL"))['host'],
+       'port' => env('DB_PORT', '5432'),
+       'database' => @substr(parse_url(getenv("DATABASE_URL"))['path'],1),
+       'username' => @parse_url(getenv("DATABASE_URL"))['user'],
+       'password' => @parse_url(getenv("DATABASE_URL"))['pass'],
+       'charset' => 'utf8',
+       'prefix' => '',
+       'schema' => 'public',
+       'sslmode' => 'prefer',
+     ],
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
