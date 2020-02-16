8eb1346c66f2fee3018543759b8af5e9f55bca52
Ramses Cruz <rcru@devolabs.io>
2020-02-15 19:17:34 -0500

    Agregdao Link Devolabs

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -25,30 +25,15 @@
             </div>
             <!-- Navbar items -->
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="{{ route('home') }}">
-                        <i class="ni ni-planet"></i>
-                        <span class="nav-link-inner--text">{{ __('Dashboard') }}</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="{{ route('register') }}">
-                        <i class="ni ni-circle-08"></i>
-                        <span class="nav-link-inner--text">{{ __('Register') }}</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="{{ route('login') }}">
-                        <i class="ni ni-key-25"></i>
-                        <span class="nav-link-inner--text">{{ __('Login') }}</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="{{ route('profile.edit') }}">
-                        <i class="ni ni-single-02"></i>
-                        <span class="nav-link-inner--text">{{ __('Profile') }}</span>
-                    </a>
-                </li>
+                            <li class="nav-item">
+                                <a class="nav-link nav-link-icon" href="https://www.devolabs.io">
+                                    <i class="ni ni-planet"></i>
+                                    <span class="nav-link-inner--text">{{ __('DevoLabs.io') }}</span>
+                                </a>
+                            </li>
+
+                        </ul>
+
             </ul>
         </div>
     </div>
