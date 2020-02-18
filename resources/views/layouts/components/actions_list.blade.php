d9ff983beb83c19697ad409bbfdc00afeb1c312e
Ramses Cruz <rcru@devolabs.io>
2020-02-18 18:54:23 -0500

    Agregados componentes del layaout

---
+++ resources/views/layouts/components/actions_list.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ route($route) }}" class="btn btn-icon btn-sm btn-outline-primary" >
+  <span class="btn-inner--icon"><i class="fa fa-plus"></i></span>
+  <span class="btn-inner--text">{{__("$title")}}</span>
+</a>
